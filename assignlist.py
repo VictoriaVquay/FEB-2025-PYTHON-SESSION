c7e245357dbab5badcfb33f1056c5dea46d4b3f7
VictoriaVquay <victoria.mwende@yahoo.com>
2025-03-20 09:43:11 +0300

    Add files via upload

---
+++ assignlist.py
@@ -0,0 +1,41 @@
+# An empty list
+my_list = []
+
+#print (my_list)
+
+# Append the following elements: 10, 20, 30, 40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#print(my_list)
+
+# Insert the value 15 at the second position (index 1 in Python)
+my_list.insert(1, 15)
+
+#print(my_list)
+
+# Extend my_list with another list: [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+#print(my_list)
+
+# Remove the last element from my_list
+my_list.pop()
+
+#print(my_list)
+
+# Sort my_list in ascending order
+my_list.sort()
+
+#print(my_list)
+
+# Find and print the index of the value 30 in my_list
+index_of_30 = my_list.index(30)
+print("The index of the value 30 in my_list is:", index_of_30)
+
+# Display the sorted list
+#print("The sorted list is:", my_list)
+
+
